117a6c4abb568915849a844638d136925c848f29
jfeng <jfeng@salesforce.com>
2019-04-03 09:28:38 -0700

    Oops

--- jest-tests/sum.test.js
+++ jest-tests/sum.test.js
@@ -3,8 +3,8 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-test('adds 0 + 2 to equal 2', () => {
-  expect(sum(0, 2)).toBe(2);
+test('adds 1 + 2 to equal 3 again', () => {
+  expect(sum(1, 2)).toBe(3);
 });
 /*
 test('add undefined + 2 to be NaN', () => {
